01a3ea92d6593493d06f984b3c342c6e462f750c
Carl Jalal <carljalal@gmail.com>
2022-10-05 19:18:34 -0400

    mostly completed, working lettermole

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const elPrompt = document.querySelector('#prompt')
 
 let result = 0
 let hitPosition
-let currentTime = 1000
+let currentTime = 100
 let timerId = null
 let numTrials = 0;
 let timeperformance = 0.0;
@@ -17,6 +17,11 @@ let lastPerformance = 0.0;
 let performanceHistory =[];
 
 let timed = true;
+let promptOnly = true;
+
+
+let lastKeyPressed = "";
+let lastSquareClicked = {};
 
 keyboardKeys = [];
 keyboardKeys.push("Backspace");
@@ -87,41 +92,55 @@ return items[Math.floor(Math.random()*items.length)];
 
 currentIterm = "";
 
+//squares.style.fontSize = 50;
+
 squares.forEach(square => {
-  square.style.fontSize = 50;
+  square.style.fontSize = "50";
   square.addEventListener('mousedown', () => {
-    if (square.id == hitPosition) {
-      timeperformance = performance.now(); 
-      if(result > 0){
-        
-        performanceHistory.push(timeperformance - lastPerformance);
-      }
+    lastSquareClicked = square;
+    if (square.id == hitPosition && currentItem == lastKeyPressed) {
       
-      result++
-      score.textContent = result + " / " + numTrials  + " trials"
-      hitPosition = null
-      square.classList.remove('mole')
-      square.classList.add('cleared')
-      if (timed == false) randomSquare();
+      scored(square);
+      
     }
   })
 })
 if (timed == false) randomSquare();
 
-
+scored = (square) => {
+  lastSquareClicked = {}
+  lastKeyPressed = "";
+  timeperformance = performance.now(); 
+  if(result > 0){
+        
+    performanceHistory.push(timeperformance - lastPerformance);
+  }
+  result++
+  score.textContent = result + " / " + numTrials  + " trials"
+  hitPosition = null
+  square.classList.remove('mole')
+  square.classList.add('cleared')
+  if (timed == false) randomSquare();
+}
 
 function randomSquare() {
-  
+  lastKeyPressed = "";
   lastPerformance = performance.now();
   squares.forEach(square => {
     square.classList.remove('mole')
     square.classList.remove('cleared')
+    square.textContent = ""
+    
   })
 
   let randomSquare = squares[Math.floor(Math.random() * 16)]
-  //randomSquare.classList.add('mole')
+  //
   currentItem = random_item(keyboardKeys);
-  randomSquare.textContent = currentItem;
+  elPrompt.innerHTML = currentItem
+
+  if (promptOnly) randomSquare.classList.add('mole')
+  else randomSquare.textContent = currentItem;
+  
   hitPosition = randomSquare.id
 }
 
@@ -152,7 +171,15 @@ function countDown() {
 }
 
 
-let countDownTimerId = setInterval(countDown, 400)
+document.body.addEventListener("keydown", (event) => {
+ lastKeyPressed = event.key;
+
+ if (lastKeyPressed == currentItem && lastSquareClicked.id == hitPosition){
+    scored(lastSquareClicked)
+ }
+})
+
+let countDownTimerId = setInterval(countDown, 1500)
 //let timeoutId = setTimeout(countDown, 1000)
 
 
